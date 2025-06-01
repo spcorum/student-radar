fix discriminator conditioning - broadcast and concatenate the label across the time axis

paper/src/models/hyperparemeters/model_cwgangp_improved.py
@@ -9,8 +9,8 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '
 from tensorflow.keras.layers import Dense, Reshape, Flatten, Conv1DTranspose, ReLU, Conv1D, LeakyReLU, InputLayer, Concatenate, LayerNormalization
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import Input
-from tensorflow.keras import Model
+from tensorflow.keras import Input, Model
+import tensorflow as tf 
 
 from cwgangp import CWGANGP
 
@@ -58,28 +58,41 @@ def build_generator(codings_size, label_dim):
 
     return Model([noise_input, label_input], output, name="generator")
 
+
+def build_discriminator(input_shape, label_dim=1):
+    # Input: radar signal
+    signal_input = Input(shape=input_shape, name="signal_input")
+    # Input: label broadcasted across time
+    label_input = Input(shape=(input_shape[0], label_dim), name="label_input")
+
+    # Concatenate along channel dimension
+    x = Concatenate(axis=-1)([signal_input, label_input])  # (1024, 17)
+
+    x = Conv1D(32, kernel_size=25, strides=4, padding='same')(x)
+    x = LeakyReLU(0.2)(x)
+    x = Conv1D(64, kernel_size=25, strides=4, padding='same')(x)
+    x = LeakyReLU(0.2)(x)
+    x = Conv1D(128, kernel_size=25, strides=4, padding='same')(x)
+    x = LeakyReLU(0.2)(x)
+    x = Conv1D(256, kernel_size=25, strides=4, padding='same')(x)
+    x = LeakyReLU(0.2)(x)
+
+    x = Flatten()(x)
+    output = Dense(1, activation='linear')(x)
+
+    return Model([signal_input, label_input], output, name='discriminator')
+
 def build_gan():
     BATCH_SIZE = 32
     EPOCHS = 1000
-
     CODINGS_SIZE = 100
+    SIGNAL_LENGTH = 1024
+    NUM_CHANNELS = 16
     LABEL_DIM = 1
 
     generator = build_generator(codings_size = CODINGS_SIZE, label_dim = LABEL_DIM)
 
-    discriminator = Sequential([
-        InputLayer((1024, 17)),
-        Conv1D(32, kernel_size=25, strides=4, padding='same',  input_shape=[1024, 16]),
-        LeakyReLU(0.2),
-        Conv1D(64, kernel_size=25, strides=4, padding='same'),
-        LeakyReLU(0.2),
-        Conv1D(128, kernel_size=25, strides=4, padding='same'),
-        LeakyReLU(0.2),
-        Conv1D(256, kernel_size=25, strides=4, padding='same'),
-        LeakyReLU(0.2),
-        Flatten(),
-        Dense(1, activation='linear'),
-    ], name='discriminator')
+    discriminator = build_discriminator(input_shape=(SIGNAL_LENGTH, NUM_CHANNELS), label_dim=LABEL_DIM)
 
     gan = CWGANGP(discriminator, generator, CODINGS_SIZE, discriminator_extra_steps=5)
 
@@ -97,9 +110,11 @@ def build_gan():
         #fake_signal = generator(gen_input)
         fake_signal = generator([dummy_noise, dummy_label])
 
-        dummy_cond = tf.zeros((1, 1024, 1))
-        disc_input = tf.concat([fake_signal, dummy_cond], axis=2)
-        _ = discriminator(disc_input)
+        #dummy_cond = tf.zeros((1, 1024, 1))
+        #disc_input = tf.concat([fake_signal, dummy_cond], axis=2)
+        #_ = discriminator(disc_input)
+        label_broadcast = tf.repeat(dummy_label[:, tf.newaxis, :], repeats=SIGNAL_LENGTH, axis=1)
+        _ = discriminator([fake_signal, label_broadcast])
 
         print("[INFO] Generator and discriminator successfully built.")
     except Exception as e: