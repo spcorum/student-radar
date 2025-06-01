dbb65f7245da32fc85b1bda642458390df4b779a
Sean Corum <spcorum@gmail.com>
2025-05-31 20:16:50 -0500

    comment out old load_dataset_labeled

--- paper/src/models/trainings/train_conditional.py
+++ paper/src/models/trainings/train_conditional.py
@@ -27,25 +27,25 @@ def load_weights(gan, weights_path):
     gan.load_weights(weights_path)
     return gan
 
-def load_dataset_labaled(path_data, path_label, batch_size):
-    print(path_data) 
-    data = np.load(path_data)
-    labels_generator, labels_discriminator = np.load(path_label, allow_pickle=True)
+# def load_dataset_labaled(path_data, path_label, batch_size):
+#     print(path_data) 
+#     data = np.load(path_data)
+#     labels_generator, labels_discriminator = np.load(path_label, allow_pickle=True)
 
-    dataset = tf.data.Dataset.from_tensor_slices((
-        tf.convert_to_tensor(data),
-        tf.convert_to_tensor(labels_generator),
-        tf.convert_to_tensor(labels_discriminator)
-    ))
+#     dataset = tf.data.Dataset.from_tensor_slices((
+#         tf.convert_to_tensor(data),
+#         tf.convert_to_tensor(labels_generator),
+#         tf.convert_to_tensor(labels_discriminator)
+#     ))
 
-    del data, labels_generator, labels_discriminator
-    gc.collect()
+#     del data, labels_generator, labels_discriminator
+#     gc.collect()
 
-    dataset = dataset.shuffle(10_000)
-    dataset = dataset.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
-    print(f'\nNumber of batches: {len(dataset)}')
+#     dataset = dataset.shuffle(10_000)
+#     dataset = dataset.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
+#     print(f'\nNumber of batches: {len(dataset)}')
 
-    return dataset
+#     return dataset
 
 def main():
     gan, batch_size, epochs, gan_config = build_gan()
