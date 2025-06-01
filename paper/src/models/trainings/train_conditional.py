dbeb67cb46bd370910442078d303cc92dc041084
Sean Corum <spcorum@gmail.com>
2025-05-31 22:22:34 -0500

    add generator loss callback

--- paper/src/models/trainings/train_conditional.py
+++ paper/src/models/trainings/train_conditional.py
@@ -4,6 +4,7 @@ import gc
 import numpy as np
 import tensorflow as tf
 import wandb
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 # Append custom module paths
 sys.path.append('models/callbacks/')
@@ -120,13 +121,21 @@ def main():
 
     print(f'\n\n--------------------- Run: {wandb.run.name} ---------------------------\n\n')
 
+    gen_loss_cb = ModelCheckpoint(
+        filepath=f'model-{wandb.run.id}.h5',
+        save_weights_only=True,
+        save_best_only=True,
+        monitor='val_generator_loss',
+        mode='min'
+    )
+
     gan.fit(
         train_dataset,
         validation_data=val_dataset,
         initial_epoch=wandb.run.step,
         epochs=epochs,
         batch_size=batch_size,
-        callbacks=[]
+        callbacks=[gen_loss_cb]
     )
 
 if __name__ == '__main__':
